99df8034fb264193e9b0e94e40df681a18fef0c3
Diego <dgv3105@gmail.com>
2023-03-29 21:16:59 -0600

    2.1.0

--- Proceso.java
+++ Proceso.java
@@ -1,3 +1,4 @@
+
 /*
 * Autores:
 * Diego García 22404
@@ -7,6 +8,71 @@
 * Proceso
 */
 
-public class Proceso {
+import java.util.Arrays;
+
+public class Proceso implements Comparable<Proceso>{
+    private String nombre;
+    private String usuario;
+    private Integer nice;
+    private Integer prioridad;
     
+    public Proceso(String nombre, String usuario, Integer nice){
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.nice = nice;
+        this.prioridad = nice+120;
+    }
+    
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return this.usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Integer getNice() {
+        return this.nice;
+    }
+
+    public void setNice(Integer nice) {
+        this.nice = nice;
+    }
+
+    public Integer getPrioridad() {
+        return this.prioridad;
+    }
+
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
+    
+    @Override
+    public int compareTo(Proceso otro) {
+        return this.nice.compareTo(otro.nice);
+    }
+    
+    public String toString(){
+        return (getNombre()+", "+getUsuario()+", "+getNice()+", PR="+getPrioridad());
+    }
+
+    public String procesosOrdenados(Proceso[] procesos) {
+        String orden = "";
+        Arrays.sort(procesos);
+    
+        System.out.println("Procesos ordenados por valor nice:");
+        for (Proceso proceso : procesos) {
+            orden+=proceso.toString();
+        }
+
+        return orden;
+    }
 }
