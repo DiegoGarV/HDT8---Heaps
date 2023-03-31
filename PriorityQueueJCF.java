0396b68daa2862eb3b3146a4373cebf8ddfe3425
Czar272 <110710742+Czar272@users.noreply.github.com>
2023-03-31 11:37:00 -0600

    Add files via upload

--- PriorityQueueJCF.java
+++ PriorityQueueJCF.java
@@ -14,36 +14,61 @@ public class PriorityQueueJCF<E extends Comparable<E>> implements PriorityQueueI
     protected PriorityQueue<E> data;
     
     
+    // Creating a new PriorityQueue.
     public PriorityQueueJCF(){
 		data = new PriorityQueue<E>();
 	}
 
     
+    // Returning null.
     @Override
     public E getFirst() {
         return null;
     }
 
+   /**
+    * It removes the first element from the queue.
+    * 
+    * @return The first element in the queue.
+    */
     @Override
     public E remove() {
         return data.remove();
     }
 
+   /**
+    * This function adds a value to the end of the list
+    * 
+    * @param value The value to be added to the list.
+    */
     @Override
     public void add(E value) {
         data.add(value);
     }
 
+  /**
+   * It checks if the size of the list is 0.
+   * 
+   * @return The size of the list.
+   */
     @Override
     public boolean isEmpty() {
         return size()==0;
     }
 
+   /**
+    * It returns the size of the data.
+    * 
+    * @return The size of the data array.
+    */
     @Override
     public int size() {
         return data.size();
     }
 
+    /**
+     * It clears the list.
+     */
     @Override
     public void clear() {}
     
