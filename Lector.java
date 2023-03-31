5bd00c0a4e51b5ee792f403d1cdc03ea73e753ee
Czar272 <110710742+Czar272@users.noreply.github.com>
2023-03-31 11:14:55 -0600

    Add files via upload

---
+++ Lector.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lector {
+
+    public List<List<String>> readTextFile() {
+        List<List<String>> lines = new ArrayList<>();
+        
+
+        try (BufferedReader br = new BufferedReader(new FileReader("procesos.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split(",");
+                List<String> currentLine = new ArrayList<>();
+                for (String word : words) {
+                    currentLine.add(word.trim());
+                }
+                lines.add(currentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }    
+}
